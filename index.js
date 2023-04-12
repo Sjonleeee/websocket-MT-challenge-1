f5d7219167d5926401651bb413f25a070ecc6058
Minh Tri Ha <minhtrihaa@gmail.com>
2023-04-12 13:16:01 +0200

    Add security https

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const https = require("https");
+const port = process.env.PORT || 443;
+const fs = require("fs");
+
+// Const options - key en crt
+const options = {
+  key: fs.readFileSync("localhost.key"),
+  cert: fs.readFileSync("localhost.crt"),
+};
+
+// Security for Https
+const server = https.Server(options, app);
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
