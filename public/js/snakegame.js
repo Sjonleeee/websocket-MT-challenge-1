693302f4d8e2b75d182817e21103af023e549f32
Minh Tri Ha <minhtrihaa@gmail.com>
2023-04-18 20:13:41 +0200

    Fix red LED light

--- public/js/snakegame.js
+++ public/js/snakegame.js
@@ -1,3 +1,173 @@
+// Arduino info
+const arduinoInfo = { usbProductId: 32823, usbVendorId: 9025 };
+let connectedArduinoPorts = [];
+
+// App state
+const hasWebSerial = "serial" in navigator;
+let isConnected = false;
+let port;
+
+const $notSupported = document.getElementById("not-supported");
+const $supported = document.getElementById("supported");
+const $notConnected = document.getElementById("not-connected");
+const $connected = document.getElementById("connected");
+
+const $connectButton = document.getElementById("connectButton");
+
+const $xValue = document.getElementById("xValue");
+const $yValue = document.getElementById("yValue");
+
+let writer;
+
+const init = async () => {
+  displaySupportedState();
+  if (!hasWebSerial) return;
+  displayConnectionState();
+
+  navigator.serial.addEventListener("connect", async (e) => {
+    console.log("connect", e.target);
+    const info = e.target.getInfo();
+    if (
+      info.usbProductId === arduinoInfo.usbProductId &&
+      info.usbVendorId === arduinoInfo.usbVendorId
+    ) {
+      await connect(e.target);
+    }
+  });
+
+  // PORTS
+  const ports = (await navigator.serial.getPorts()).filter((port) => {
+    const info = port.getInfo();
+    return (
+      info.usbProductId === arduinoInfo.usbProductId &&
+      info.usbVendorId === arduinoInfo.usbVendorId
+    );
+  });
+  if (ports.length > 0) {
+    await connect(ports[0]);
+  }
+  console.log(ports);
+  $connectButton.addEventListener("click", handleClickConnect);
+
+  // REQUEST PORT
+  const port = await navigator.serial.requestPort();
+  console.log(port);
+};
+
+const handleClickConnect = async () => {
+  port = await navigator.serial.requestPort();
+  console.log(port);
+  const info = port.getInfo();
+  console.log(info);
+};
+
+// Update the connected state
+const connect = async (port) => {
+  isConnected = true;
+  displayConnectionState();
+  await port.open({ baudRate: 9600 });
+
+  // Sending data to the Arduino
+  const textEncoder = new TextEncoderStream();
+  const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+  writer = textEncoder.writable.getWriter();
+
+  // Linebreak transformer
+  const lineBreakTransformer = new TransformStream({
+    transform(chunk, controller) {
+      const text = chunk;
+      const lines = text.split("\n");
+      lines[0] = (this.remainder || "") + lines[0];
+      this.remainder = lines.pop();
+      lines.forEach((line) => controller.enqueue(line));
+    },
+    flush(controller) {
+      if (this.remainder) {
+        controller.enqueue(this.remainder);
+      }
+    },
+  });
+
+  // Read data from the port
+  while (port.readable) {
+    const decoder = new TextDecoderStream();
+    const readableStreamClosed = port.readable.pipeTo(decoder.writable);
+    const inputStream = decoder.readable.pipeThrough(lineBreakTransformer);
+    const reader = inputStream.getReader();
+
+    try {
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          // |reader| has been canceled.
+          break;
+        }
+        try {
+          const parsed = JSON.parse(value);
+          processJSON(parsed);
+        } catch (e) {
+          // console.log(e);
+        }
+      }
+    } catch (error) {
+      // Handle |error|...
+    } finally {
+      reader.releaseLock();
+    }
+  }
+
+  // If that port is closed, stop sending data
+  port.addEventListener("disconnect", () => {
+    console.log(`Disconnected: ${port.serialNumber}`);
+    lineBreakTransformer.readable.cancel();
+    isConnected = false;
+    displayConnectionState();
+  });
+};
+
+const processJSON = (json) => {
+  if (json.sensor === "joystick") {
+    const joystickX = json.data[0];
+    const joystickY = json.data[1];
+
+    // Determine direction based on joystick input
+    if (joystickX > 600) {
+      direction = "right";
+    } else if (joystickX < 400) {
+      direction = "left";
+    } else if (joystickY > 600) {
+      direction = "down";
+    } else if (joystickY < 400) {
+      direction = "up";
+    }
+
+    $xValue.innerText = "x: " + joystickX;
+    $yValue.innerText = "y: " + joystickY;
+  }
+};
+
+const displaySupportedState = () => {
+  if (hasWebSerial) {
+    $notSupported.style.display = "none";
+    $supported.style.display = "block";
+  } else {
+    $notSupported.style.display = "block";
+    $supported.style.display = "none";
+  }
+};
+
+const displayConnectionState = () => {
+  if (isConnected) {
+    $notConnected.style.display = "none";
+    $connected.style.display = "block";
+  } else {
+    $notConnected.style.display = "block";
+    $connected.style.display = "none";
+  }
+};
+
+init();
+
 let direction = "right";
 let startSnake = [
   { x: 300, y: 300 },
@@ -28,7 +198,7 @@ const initGame = () => {
   moveSnake();
 };
 
-const drawSnake = () => {
+const drawSnake = async () => {
   snake.forEach((segment) => {
     const snakeBody = document.createElement("div");
     snakeBody.style.left = segment.x + "px";
@@ -83,7 +253,17 @@ const checkCollision = (head) => {
   return false;
 };
 
-const gameOver = () => {
+const gameOver = async () => {
+  if (!isConnected) {
+    return;
+  }
+  await writer.write(
+    JSON.stringify({
+      red: "R",
+    })
+  );
+  await writer.write("\n");
+
   clearInterval(game);
   gameArea.innerHTML = "";
   const gameOver = document.createElement("h1");
